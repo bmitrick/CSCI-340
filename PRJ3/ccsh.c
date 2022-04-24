32fd740155d8e41bc01c4a8e7c56c8e3d03e7562
bmitrick <bmitrick@gmail.com>
2022-04-24 17:36:45 -0400

    working on getting the execute working

--- PRJ3/ccsh.c
+++ PRJ3/ccsh.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 //GLOBAL DATA
-char *exitCode = "exit\n";
+char *exitCode = "exit";
 
-void execute(char *line){
-  printf("%s", line);
+void displayError(){
+  char error_message[30] = "An error has occurred\n";
+  write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-void interativeMode(){
+void execute(char *line){
+  //printf("user input %s \n", line);
+
+  char* str;
+  str = (char*)malloc(strlen(line));
+  strcpy(str, line);
+
+  //printf("user str %s \n", str);
+
+  char delim[] = " ";
+
+  char *ptr = strtok(str, delim);
+
+  int argNums = 0;
+  while (ptr != NULL){
+    argNums++;
+    //printf("input = %s \n", ptr);
+    ptr = strtok(NULL, delim);
+  }
+
+  strcpy(str, line);
+  //printf("%s \n", str);
+  ptr = strtok(str, delim);
+  char *args[argNums + 1];
+  int i = 0;
+  while (ptr != NULL){
+    //printf("input %d = %s \n", i, ptr);
+    args[i] = ptr;
+    i++;
+    ptr = strtok(NULL, delim);
+  }
+  args[i] = NULL;
+
+  execv(args[0], args);
+}
+
+void interactiveMode(){
   printf("ccsh> ");
 
   char *line = NULL;
@@ -17,16 +55,27 @@ void interativeMode(){
 
   getline(&line, &len, stdin);
 
+  //Trim the \n from the line
+  line[strcspn(line, "\n")] = 0;
+
   //If the command entered is the exit code, exit the program
   if(strcmp(line, exitCode) == 0){
     exit(0);
   }
 
-  //Execute line
-  execute(line);
+  pid_t pid = fork();
 
-  //Call interative mode again to pull new line data
-  interativeMode();
+  if (pid < 0){
+    printf("error in fork");
+    exit(0);
+  }
+  else if (pid == 0) {
+    execute(line);
+  }
+  else{
+    wait(NULL);
+    interactiveMode();
+  }
 }
 
 void batchMode(char *fileName){
@@ -40,7 +89,7 @@ int main(int argc, char *argv[]){
   //See what mode the user has entered
   if(argc == 1){
     //If the user does not input a file name, the shell enters interative mode
-    interativeMode();
+    interactiveMode();
   } else {
     //If the user does input a file name, the shell enters batch mode & passes the file name to the batch mode terminal
     char *batchFile = argv[1];
